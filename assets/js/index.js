8ffddbfef13ac34a4a19602d52dc17ca0203a607
Sasha <x6rrl@yandex.ru>
2023-08-31 09:30:43 +1000

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,57 @@
-// change button state
+window.addEventListener("DOMContentLoaded", function() {
+    
+    const mainBlock = document.querySelector('.button-wrap');
+    const buttonCircle = mainBlock.querySelector('.circle');
+    
+    const rangeInput = document.querySelector('input[type="range"]')
+    const outputResult = document.querySelector('.right-side');
+    const pageView = document.querySelector('.left-side');
+    const min = 8
+    const minPage = 10;
+    let mainBlockState = false;
 
-const mainBlock = document.querySelector('.button-wrap');
-const buttonCircle = mainBlock.querySelector('.circle');
+    rangeInput.value = 0
+    outputResult.innerHTML = `$${min}<span>/ month</span>`;
+    pageView.innerHTML =   `${minPage}K pageviews`;
 
-const rangeInput = document.querySelector('input[type="range"]')
+    // toggle button slide and state button
+    mainBlock.addEventListener('click', function() {
+        buttonCircle.classList.toggle('change')
+        mainBlock.classList.toggle('changeColor')
+        mainBlockState = !mainBlockState
+        if(mainBlockState === true) {
+            salePrice()
+        }
+    });
 
-mainBlock.addEventListener('click', function() {
-    buttonCircle.classList.toggle('change')
-    mainBlock.classList.toggle('changeColor')
-})
+    // range result
+    rangeInput.addEventListener('input', function() {
+        rangeResult(rangeInput.value)
+    });
+    function rangeResult() {
+        htmlTemplate = `$${rangeInput.value}<span>/ month</span>`
+        switch(rangeInput.value) {
+            case "12":
+                pageView.innerHTML =  `${50}K pageviews`;
+                break;
+            case "16":
+                pageView.innerHTML =  `${100}K pageviews`;
+                break;
+            case "24":
+                pageView.innerHTML =  `${500}K pageviews`;
+                break;
+            case "36":
+                pageView.innerHTML =  `${1}M pageviews`;
+                break;
+            case "8":
+                pageView.innerHTML =   `${10}K pageviews`;
+                break;
+            default:
+                break;
+        }
+        outputResult.innerHTML = htmlTemplate
+    };
 
-rangeInput.addEventListener('input', function() {
-    console.log(rangeInput.value)
-})
+});
+
+// pageView result 
